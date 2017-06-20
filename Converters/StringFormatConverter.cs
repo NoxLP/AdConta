7e7247802b76e46deb9e36076fe453f96856e7e5
NoxLP <nox_nox_@hotmail.com>
2017-06-20 09:50:51 +0100

    20/06/2017 WTF

---
+++ Converters/StringFormatConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converters
+{
+    /// <summary>
+    /// General converter for strings.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(string))]
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string param = parameter as string;
+
+            if (value == null || targetType == null)
+                return null;
+            else if (parameter == null)
+                return value.ToString();
+            else if (param.Contains("PADLEFT"))
+            {
+                int n = 0;
+
+                if (int.TryParse(param.Substring(7), out n))
+                    return value.ToString().PadLeft(n, '0');
+                else
+                    return value.ToString().PadLeft(4, '0');
+            }
+            else if (param.Contains("DATE"))
+            {
+                string format = param.Substring(4);
+
+                if (value is DateTime)
+                    return ((DateTime)value).ToString(format, System.Globalization.CultureInfo.CurrentCulture);
+                else throw new Exception("Converter value supossed to be a DateTime is NOT a DateTime");
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Repository/AdContaContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using AdConta.Models;
+using ModuloContabilidad.ObjModels;
+using ModuloGestion.ObjModels;
+
+namespace EFDBContext
+{
+    class AdContaContext : DbContext
+    {
+        #region general
+        public virtual DbSet<Persona> Personas { get; set; }
+        public virtual DbSet<Concepto> Conceptos { get; set; }
+        public virtual DbSet<Ejercicio> Ejercicios { get; set; }
+        public virtual DbSet<DireccionPostalCompleta> Direcciones { get; set; }
+        //OJO FALTA COMUNIDAD
+        #endregion
+
+        #region contabilidad
+        public virtual DbSet<Proveedor> Proveedores { get; set; }
+        public virtual DbSet<Apunte> Apuntes { get; set; }
+        public virtual DbSet<Factura> Facturas { get; set; }
+        public virtual DbSet<Gasto> Gastos { get; set; }
+        public virtual DbSet<GruposCuentas> GruposCuentas { get; set; }
+        public virtual DbSet<CuentaMayor> CuentasMayor { get; set; }
+        public virtual DbSet<Pago> Pagos { get; set; }
+        #endregion
+
+        #region gestion
+        public virtual DbSet<Cuota> Cuotas { get; set; }
+        public virtual DbSet<Devolucion> Devoluciones { get; set; }
+        public virtual DbSet<Finca> Fincas { get; set; }
+        public virtual DbSet<Recibo> Recibos { get; set; }
+        public virtual DbSet<Cobro> Cobros { get; set; }
+        public virtual DbSet<EntACta> EntregasACta { get; set; }
+        #endregion
+    }
+}
